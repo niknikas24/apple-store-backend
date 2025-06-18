1274148730b5cebb64da1b116b3814eec9f5f378
niknikas24 <kvadrolis@gmail.com>
2025-06-18 20:23:14 +0300

    Create types.js

---
+++ dist/types/types.js
@@ -0,0 +1,15 @@
+// export interface Gadget {
+//     name: string;
+//     price: number;
+//     discountPrice?: number;
+//     guarantee: number;
+//     rating: number;
+//     countReview: number;
+//     isAvailable: true;
+//     storeAddress?: string;
+//     color: string;
+//     brand: string;
+//     country: string;
+// }
+// export type ProuductCategories = 'phone' | 'tablet' | 'headphones' | 'computer' | 'accessories'
+//# sourceMappingURL=types.js.map
